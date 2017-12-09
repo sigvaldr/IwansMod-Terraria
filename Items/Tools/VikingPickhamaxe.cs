d0a170896d98a6a3a296e8b852ae1726edad00e6
BuildTools <vikingiwan@gmail.com>
2017-12-09 17:59:02 -0500

    Added Muzm boss. No AI yet

---
+++ Items/Tools/VikingPickhamaxe.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace IwansMod.Items.Tools
+{
+    class VikingPickhamaxe : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Viking Pickhamaxe");
+            Tooltip.SetDefault("The only tool you'll need!");
+        }
+
+        public override void SetDefaults()
+        {
+
+
+            item.CloneDefaults(ItemID.AdamantitePickaxe);
+            item.useTime = 20; // Speed before reuse
+            item.rare = 10; // Item Tier
+            item.autoReuse = true; // Do you want to torture people with clicking? Set to false
+
+            item.pick = 190; // Pick Power - Higher Value = Better
+            item.axe = 200;
+            item.hammer = 200;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(mod.ItemType("VikingMythMetal"), 20);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+    }
+}
---
+++ NPCs/AncientViking.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace IwansMod.NPCs
+{
+    class AncientViking : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Ancient Viking");
+        }
+
+        public override void SetDefaults()
+        {
+            npc.CloneDefaults(NPCID.ArmoredViking);
+            npc.damage = 200;
+            npc.defense = 5;
+            npc.lifeMax = 2000;
+            npc.knockBackResist = 0.75f;
+            Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.ArmoredViking]; //Main.npcFrameCount[3];
+            aiType = NPCID.ArmoredViking; // aiType = 3;
+            animationType = NPCID.ArmoredViking; // animationType = 3;
+        }
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            return SpawnCondition.OverworldNightMonster.Chance * 0.50f;
+        }
+
+        public override void NPCLoot()
+        {
+            if (Main.rand.Next(1001) == 0)
+                Item.NewItem(npc.getRect(), mod.ItemType("BlazeYoyo"), 1);
+        }
+    }
+}
